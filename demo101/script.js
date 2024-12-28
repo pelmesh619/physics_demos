54513e20be752add25b54368f37fa571f700d542
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-12-28 10:52:22 +0300

    update to new api

--- demo101/script.js
+++ demo101/script.js
@@ -1,18 +1,13 @@
 function main() {
     var coordinatesSystemForm = new FormMaker("coordinatesSystemForm");
 
-    coordinatesSystemForm
+    coordinatesSystemForm.AddParagraph("Из системы:")
     .AddRadio(
-        new RadioInput("isometric", "outputProjection", "Изометрическая", true)
-    )
-    .AddRadio(
-        new RadioInput("dimetric", "outputProjection", "Диметрическая")
+        new RadioInput("cartesian", "coordinatesType", "прямоугольной", true, updateCoordinatesValuesForm)
     ).AddRadio(
-        new RadioInput("cartesian", "coordinatesType", "Прямоугольная система", true, updateCoordinatesValuesForm)
+        new RadioInput("cylindical", "coordinatesType", "цилиндрической", false, updateCoordinatesValuesForm)
     ).AddRadio(
-        new RadioInput("cylindical", "coordinatesType", "Цилиндрическая система", false, updateCoordinatesValuesForm)
-    ).AddRadio(
-        new RadioInput("spherical", "coordinatesType", "Сферическая система", false, updateCoordinatesValuesForm)
+        new RadioInput("spherical", "coordinatesType", "сферической", false, updateCoordinatesValuesForm)
     ).AddChildForm("coordinatesValuesForm")
     .AddChildForm("coordinatesOutputForm")
     .AddSubmitButton("submit", "Подтвердить", (v) => { window.alert(JSON.stringify(v)); })
@@ -28,23 +23,23 @@ function updateCoordinatesValuesForm(e) {
 
     if (valuesType == "cartesian") {
         coordinatesValuesFrom
-        .AddNumber(new NumberInput("x", "x = ", "м", 0.001))
-        .AddNumber(new NumberInput("y", "y = ", "м", 0.001))
-        .AddNumber(new NumberInput("z", "z = ", "м", 0.001))
+        .AddNumber(new NumberInput("x", "x = ", new NumberDomain(0, "м", 0.001)))
+        .AddNumber(new NumberInput("y", "y = ", new NumberDomain(0, "м", 0.001)))
+        .AddNumber(new NumberInput("z", "z = ", new NumberDomain(0, "м", 0.001)))
     } else if (valuesType == "cylindical") {
         coordinatesValuesFrom
-        .AddNumber(new NumberInput("r", "r = ", "м", 0.001))
-        .AddNumber(new NumberInput("phi", "phi = ", "rad", 0.001))
-        .AddNumber(new NumberInput("phi_deg", "", "deg", 0.001))
-        .AddNumber(new NumberInput("z", "z = ", "м", 0.001))
+        .AddNumber(new NumberInput("r", "r = ", new NumberDomain(0, "м", 0.001)))
+        .AddNumber(new NumberInput("phi", "phi = ", new NumberDomain(0, "rad", 0.001)))
+        .AddNumber(new NumberInput("phi_deg", "", new NumberDomain(0, "deg", 0.001)))
+        .AddNumber(new NumberInput("z", "z = ", new NumberDomain(0, "м", 0.001)))
         .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; })
     } else if (valuesType == "spherical") {
         coordinatesValuesFrom
-        .AddNumber(new NumberInput("r", "r = ", "м", 0.001))
-        .AddNumber(new NumberInput("phi", "phi = ", "rad", 0.001))
-        .AddNumber(new NumberInput("phi_deg", "", "deg", 0.001))
-        .AddNumber(new NumberInput("theta", "theta = ", "rad", 0.001))
-        .AddNumber(new NumberInput("theta_deg", "", "deg", 0.001))
+        .AddNumber(new NumberInput("r", "r = ", new NumberDomain(0, "м", 0.001)))
+        .AddNumber(new NumberInput("phi", "phi = ", new NumberDomain(0, "rad", 0.001)))
+        .AddNumber(new NumberInput("phi_deg", "", new NumberDomain(0, "deg", 0.001)))
+        .AddNumber(new NumberInput("theta", "theta = ", new NumberDomain(0, "rad", 0.001)))
+        .AddNumber(new NumberInput("theta_deg", "", new NumberDomain(0, "deg", 0.001)))
         .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; })
         .ConnectNumbers('theta', 'theta_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; });
     }
