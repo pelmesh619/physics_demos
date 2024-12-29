43394ab010ed280297efaffe3c1879be6665faeb
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2024-12-29 22:52:21 +0300

    Create general.js

---
+++ general.js
@@ -0,0 +1,7 @@
+function showModelInfo() {
+    document.getElementById('hintText').style.display = "block";
+}
+
+function hideModelInfo() {
+    document.getElementById('hintText').style.display = "none";
+}
