7aaf993acc83834c6a3722c8733cb0baffc33589
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-01-20 03:55:19 +0300

    feat hint pin by clicking

--- general.js
+++ general.js
@@ -1,7 +1,33 @@
-function showModelInfo() {
-    document.getElementById('hintText').style.display = "block";
-}
+class HintTextShower {
+    static _enable = false;
 
-function hideModelInfo() {
-    document.getElementById('hintText').style.display = "none";
+    static get enable() {
+        return HintTextShower._enable;
+    }
+
+    static set enable(value) {
+        if (value) {
+            document.getElementById('hintText').style.display = "block";
+            document.getElementById('hintIcon').classList.add('activeHintIcon');
+            HintTextShower._enable = true;
+        } else {
+            document.getElementById('hintText').style.display = "none";
+            document.getElementById('hintIcon').classList.remove('activeHintIcon');
+            HintTextShower._enable = false;
+        }
+    }
+
+    static showModelInfo() {
+        if (!HintTextShower.enable) {
+            document.getElementById('hintText').style.display = "block";
+            document.getElementById('hintIcon').classList.add('activeHintIcon');
+        }
+    }
+
+    static hideModelInfo() {
+        if (!HintTextShower.enable) {
+            document.getElementById('hintText').style.display = "none";
+            document.getElementById('hintIcon').classList.remove('activeHintIcon');
+        }
+    }
 }
