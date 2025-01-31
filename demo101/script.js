98ce40876f56624f4784f4320c7a62c2b8afe4fb
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-01-31 22:30:33 +0300

    update demo101

--- demo101/script.js
+++ demo101/script.js
@@ -1,116 +1,133 @@
-function main() {
-    var coordinatesSystemForm = new FormMaker("coordinatesSystemForm");
 
-    coordinatesSystemForm.AddParagraph("Из системы:")
-    .AddRadio(
-        new RadioInput("cartesian", "coordinatesType", "прямоугольной", true, updateCoordinatesValuesForm)
-    ).AddRadio(
-        new RadioInput("cylindrical", "coordinatesType", "цилиндрической", false, updateCoordinatesValuesForm)
-    ).AddRadio(
-        new RadioInput("spherical", "coordinatesType", "сферической", false, updateCoordinatesValuesForm)
-    ).AddChildForm("coordinatesValuesForm")
-    .AddChildForm("coordinatesOutputForm");
+class Main {
+    main() {
+        this.coordinatesSystemForm = new FormMaker("coordinatesSystemForm");
+        this.coordinatesOutputForm = new FormMaker("coordinatesOutputForm");
+        this.coordinatesValuesForm = new FormMaker("coordinatesValuesForm");
 
-    updateCoordinatesValuesForm();
-}
+        this.coordinatesSystemForm.AddParagraph("Из системы:")
+        .AddRadio(
+            new RadioInput("cartesian", "coordinatesType", "прямоугольной", true, this.updateCoordinatesValuesFormFactory())
+        ).AddRadio(
+            new RadioInput("cylindrical", "coordinatesType", "цилиндрической", false, this.updateCoordinatesValuesFormFactory())
+        ).AddRadio(
+            new RadioInput("spherical", "coordinatesType", "сферической", false, this.updateCoordinatesValuesFormFactory())
+        ).AddChildForm(this.coordinatesValuesForm)
+        .AddChildForm(this.coordinatesOutputForm);
 
-function updateDisplays(event) {
-    const values = (new FormMaker("coordinatesSystemForm")).GetValues();
-    const coordinatesOutputFrom = new FormMaker("coordinatesOutputForm");
+        this.updateCoordinatesValuesForm();
+    }
 
-    const x = coordinatesOutputFrom.GetElement('x_display');
-    const y = coordinatesOutputFrom.GetElement('y_display');
-    const z = coordinatesOutputFrom.GetElement('z_display');
-    const zeta = coordinatesOutputFrom.GetElement('zeta_display');
-    const r = coordinatesOutputFrom.GetElement('r_display');
-    const phi = coordinatesOutputFrom.GetElement('phi_display');
-    const rho = coordinatesOutputFrom.GetElement('rho_display');
-    const alpha = coordinatesOutputFrom.GetElement('alpha_display');
-    const theta = coordinatesOutputFrom.GetElement('theta_display');
+    updateDisplaysFactory() {
+        let t = this;
+        return () => { t.updateDisplays(); }
+    }
 
-    if (values["coordinatesType"] == "cartesian") {
-        zeta.innerText = values['z'] + ' м';
-        r.innerText = toScientificNotation(Math.hypot(values['x'], values['y'])) + ' м';
-        phi.innerText = toScientificNotation(Math.atan2(values['y'], values['x'])) + ' rad';
-        let rhoValue = Math.hypot(values['x'], values['y'], values['z']);
-        rho.innerText = toScientificNotation(rhoValue) + ' м';
-        alpha.innerText = toScientificNotation(Math.atan2(values['y'], values['x'])) + ' rad';
-        theta.innerText = toScientificNotation(rhoValue != 0 ? Math.acos(values['z'] / rhoValue) : 0) + ' rad';
-    } else if (values["coordinatesType"] == "cylindrical") {
-        z.innerText = values['z'] + ' м';
-        x.innerText = toScientificNotation(Math.cos(values['phi']) * values['r']) + ' м';
-        y.innerText = toScientificNotation(Math.sin(values['phi']) * values['r']) + ' м';
-        let rhoValue = Math.hypot(values['r'], values['z']);
-        rho.innerText = toScientificNotation(rhoValue) + ' м';
-        alpha.innerText = toScientificNotation(values['phi']) + ' rad';
-        theta.innerText = toScientificNotation(rhoValue != 0 ? Math.acos(values['z'] / rhoValue) : 0) + ' rad';
-    } else if (values["coordinatesType"] == "spherical") {
-        z.innerText = toScientificNotation(values['r'] * Math.sin(values['theta'])) + ' м';
-        x.innerText = toScientificNotation(values['r'] * Math.cos(values['theta']) * Math.cos(values['phi'])) + ' м';
-        y.innerText = toScientificNotation(values['r'] * Math.cos(values['theta']) * Math.sin(values['phi'])) + ' м';
-        r.innerText = toScientificNotation(values['r'] * Math.sin(values['theta'])) + ' м';
-        phi.innerText = values['phi'] + ' rad';
-        zeta.innerText = toScientificNotation(values['r'] * Math.cos(values['theta'])) + ' м';
+    updateCoordinatesValuesFormFactory() {
+        let t = this;
+        return () => { t.updateCoordinatesValuesForm(); }
+    }
+
+    updateDisplays(event) {
+        const values = this.coordinatesSystemForm.GetValues();
+
+        const x = this.coordinatesOutputForm.GetElement('x_display');
+        const y = this.coordinatesOutputForm.GetElement('y_display');
+        const z = this.coordinatesOutputForm.GetElement('z_display');
+        const zeta = this.coordinatesOutputForm.GetElement('zeta_display');
+        const r = this.coordinatesOutputForm.GetElement('r_display');
+        const phi = this.coordinatesOutputForm.GetElement('phi_display');
+        const rho = this.coordinatesOutputForm.GetElement('rho_display');
+        const alpha = this.coordinatesOutputForm.GetElement('alpha_display');
+        const theta = this.coordinatesOutputForm.GetElement('theta_display');
+
+        if (values["coordinatesType"] == "cartesian") {
+            zeta.innerText = values['z'] + ' м';
+            r.innerText = toScientificNotation(Math.hypot(values['x'], values['y'])) + ' м';
+            phi.innerText = toScientificNotation(Math.atan2(values['y'], values['x'])) + ' rad';
+            let rhoValue = Math.hypot(values['x'], values['y'], values['z']);
+            rho.innerText = toScientificNotation(rhoValue) + ' м';
+            alpha.innerText = toScientificNotation(Math.atan2(values['y'], values['x'])) + ' rad';
+            theta.innerText = toScientificNotation(rhoValue != 0 ? Math.acos(values['z'] / rhoValue) : 0) + ' rad';
+        } else if (values["coordinatesType"] == "cylindrical") {
+            z.innerText = values['z'] + ' м';
+            x.innerText = toScientificNotation(Math.cos(values['phi']) * values['r']) + ' м';
+            y.innerText = toScientificNotation(Math.sin(values['phi']) * values['r']) + ' м';
+            let rhoValue = Math.hypot(values['r'], values['z']);
+            rho.innerText = toScientificNotation(rhoValue) + ' м';
+            alpha.innerText = toScientificNotation(values['phi']) + ' rad';
+            theta.innerText = toScientificNotation(rhoValue != 0 ? Math.acos(values['z'] / rhoValue) : 0) + ' rad';
+        } else if (values["coordinatesType"] == "spherical") {
+            z.innerText = toScientificNotation(values['r'] * Math.sin(values['theta'])) + ' м';
+            x.innerText = toScientificNotation(values['r'] * Math.cos(values['theta']) * Math.cos(values['phi'])) + ' м';
+            y.innerText = toScientificNotation(values['r'] * Math.cos(values['theta']) * Math.sin(values['phi'])) + ' м';
+            r.innerText = toScientificNotation(values['r'] * Math.sin(values['theta'])) + ' м';
+            phi.innerText = values['phi'] + ' rad';
+            zeta.innerText = toScientificNotation(values['r'] * Math.cos(values['theta'])) + ' м';
+        }
+    }
+
+    updateCoordinatesValuesForm(e) {
+        const valuesType = this.coordinatesSystemForm.GetValues()["coordinatesType"];
+        this.coordinatesValuesForm.Clear();
+        this.coordinatesOutputForm.Clear();
+
+        if (valuesType == "cartesian") {
+            this.coordinatesValuesForm
+            .AddInputObject(new NumberInput("x", "x = ", new NumberInputScheme(0, "м", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("y", "y = ", new NumberInputScheme(0, "м", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("z", "z = ", new NumberInputScheme(0, "м", 0.001), this.updateDisplaysFactory()));
+
+            this.coordinatesOutputForm.AddParagraph("В цилиндрической системе:")
+            .AddDisplay("r_display")
+            .AddDisplay("phi_display")
+            .AddDisplay("zeta_display")
+            .AddParagraph("В сферической системе:")
+            .AddDisplay("rho_display")
+            .AddDisplay("alpha_display")
+            .AddDisplay("theta_display");
+        } else if (valuesType == "cylindrical") {
+            this.coordinatesValuesForm
+            .AddInputObject(new NumberInput("r", "r = ", new NumberInputScheme(0, "м", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("phi", "phi = ", new NumberInputScheme(0, "rad", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("phi_deg", "", new NumberInputScheme(0, "deg", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("z", "z = ", new NumberInputScheme(0, "м", 0.001)))
+            .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; });
+
+            this.coordinatesOutputForm.AddParagraph("В прямоугольной системе:")
+            .AddDisplay("x_display")
+            .AddDisplay("y_display")
+            .AddDisplay("z_display")
+            .AddParagraph("В сферической системе:")
+            .AddDisplay("rho_display")
+            .AddDisplay("alpha_display")
+            .AddDisplay("theta_display");
+        } else if (valuesType == "spherical") {
+            this.coordinatesValuesForm
+            .AddInputObject(new NumberInput("r", "r = ", new NumberInputScheme(0, "м", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("phi", "phi = ", new NumberInputScheme(0, "rad", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("phi_deg", "", new NumberInputScheme(0, "deg", 0.001)))
+            .AddInputObject(new NumberInput("theta", "theta = ", new NumberInputScheme(0, "rad", 0.001), this.updateDisplaysFactory()))
+            .AddInputObject(new NumberInput("theta_deg", "", new NumberInputScheme(0, "deg", 0.001)))
+            .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; })
+            .ConnectNumbers('theta', 'theta_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; });
+
+            
+            this.coordinatesOutputForm.AddParagraph("В прямоугольной системе:")
+            .AddDisplay("x_display")
+            .AddDisplay("y_display")
+            .AddDisplay("z_display")
+            .AddParagraph("В цилиндрической системе:")
+            .AddDisplay("r_display")
+            .AddDisplay("phi_display")
+            .AddDisplay("zeta_display");
+        }
+        this.updateDisplays();
     }
 }
 
-function updateCoordinatesValuesForm(e) {
-    const coordinatesSystemForm = new FormMaker("coordinatesSystemForm");
-    const valuesType = coordinatesSystemForm.GetValues()["coordinatesType"];
-    const coordinatesValuesFrom = new FormMaker("coordinatesValuesForm").Clear();
-    const coordinatesOutputFrom = new FormMaker("coordinatesOutputForm").Clear();
+window.onload = () => {
+    var main = new Main();
 
-    if (valuesType == "cartesian") {
-        coordinatesValuesFrom
-        .AddInputObject(new NumberInput("x", "x = ", new NumberInputScheme(0, "м", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("y", "y = ", new NumberInputScheme(0, "м", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("z", "z = ", new NumberInputScheme(0, "м", 0.001), updateDisplays));
-
-        coordinatesOutputFrom.AddParagraph("В цилиндрической системе:")
-        .AddDisplay("r_display")
-        .AddDisplay("phi_display")
-        .AddDisplay("zeta_display")
-        .AddParagraph("В сферической системе:")
-        .AddDisplay("rho_display")
-        .AddDisplay("alpha_display")
-        .AddDisplay("theta_display");
-    } else if (valuesType == "cylindrical") {
-        coordinatesValuesFrom
-        .AddInputObject(new NumberInput("r", "r = ", new NumberInputScheme(0, "м", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("phi", "phi = ", new NumberInputScheme(0, "rad", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("phi_deg", "", new NumberInputScheme(0, "deg", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("z", "z = ", new NumberInputScheme(0, "м", 0.001)))
-        .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; });
-
-        coordinatesOutputFrom.AddParagraph("В прямоугольной системе:")
-        .AddDisplay("x_display")
-        .AddDisplay("y_display")
-        .AddDisplay("z_display")
-        .AddParagraph("В сферической системе:")
-        .AddDisplay("rho_display")
-        .AddDisplay("alpha_display")
-        .AddDisplay("theta_display");
-    } else if (valuesType == "spherical") {
-        coordinatesValuesFrom
-        .AddInputObject(new NumberInput("r", "r = ", new NumberInputScheme(0, "м", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("phi", "phi = ", new NumberInputScheme(0, "rad", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("phi_deg", "", new NumberInputScheme(0, "deg", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("theta", "theta = ", new NumberInputScheme(0, "rad", 0.001), updateDisplays))
-        .AddInputObject(new NumberInput("theta_deg", "", new NumberInputScheme(0, "deg", 0.001)))
-        .ConnectNumbers('phi', 'phi_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; })
-        .ConnectNumbers('theta', 'theta_deg', (x) => { return x / Math.PI * 180; }, (x) => { return x * Math.PI / 180; });
-
-        
-        coordinatesOutputFrom.AddParagraph("В прямоугольной системе:")
-        .AddDisplay("x_display")
-        .AddDisplay("y_display")
-        .AddDisplay("z_display")
-        .AddParagraph("В цилиндрической системе:")
-        .AddDisplay("r_display")
-        .AddDisplay("phi_display")
-        .AddDisplay("zeta_display");
-    }
-    updateDisplays(null, coordinatesSystemForm.GetValues());
-}
-
-window.onload = main;
+    main.main();
+};
