5841508d9c14a717289c9dbdeded1487a6bcc8bf
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-05-25 22:22:46 +0300

    feat downloadData method for export

--- demo201/script.js
+++ demo201/script.js
@@ -417,4 +417,21 @@ class ChartObserver {
     }
 }
 
+function downloadData(filename, data) {
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+}
+
+
 window.onload = main;
