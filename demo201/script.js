609d13e3056fa8fda3d09662a0c8df925af063f4
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-05-25 23:13:28 +0300

    fix units

--- demo201/script.js
+++ demo201/script.js
@@ -34,8 +34,8 @@ class Main {
         const values = this.form.GetValues();
         const settingsValues = this.settingsForm.GetValues();
 
-        document.getElementById('omega_1').innerText = toScientificNotation(Math.sqrt(Constants.g / values.L)) + ' Гц'
-        document.getElementById('omega_2').innerText = toScientificNotation(Math.sqrt(Constants.g / values.L + (2 * values.k * values.L1**2) / (values.m * values.L**2))) + ' Гц'
+        document.getElementById('omega_1').innerText = toScientificNotation(Math.sqrt(Constants.g / values.L)) + ' рад/с'
+        document.getElementById('omega_2').innerText = toScientificNotation(Math.sqrt(Constants.g / values.L + (2 * values.k * values.L1**2) / (values.m * values.L**2))) + ' рад/с'
 
         this.simulationModel = new MechanicsSimulationModel(this.form, this.renderer);
         this.simulationModel.addObject(new Grid(new Vec2(0, 0), new Vec2(20, 20)));
