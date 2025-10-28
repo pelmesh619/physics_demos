9f8380e08996e2668dff0d87ff12bcd47769f21f
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-10-28 04:11:34 +0300

    feat access methods to LinearVector

--- linalg.js
+++ linalg.js
@@ -30,6 +30,24 @@ class LinearVector {
         this._hypot = undefined;
     }
 
+    get(i) {
+        if (i >= this.length || i < 0) {
+            throw new IndexError("i is not an index for this vector");
+        }
+        return this._v[i];
+    }
+
+    at(i) {
+        return this.get(i >= 0 ? i : (i - Math.floor(i / this.length) * this.length) % this.length);
+    }
+
+    set(i, x) {
+        if (i >= this.length || i < 0) {
+            throw new IndexError("i is not an index for this vector");
+        }
+        this._v[i] = x;
+    }
+    
     copy() {
         return new LinearVector(Array.from(this._v));
     }
