4e84803fdb729c20a4f3684938b3698a7b6c4d76
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-10-28 16:07:08 +0300

    feat downloadData in general script

--- general.js
+++ general.js
@@ -31,3 +31,19 @@ class HintTextShower {
         }
     }
 }
+
+function downloadData(filename, data) {
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+}
