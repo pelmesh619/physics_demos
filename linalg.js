30ede95ec92560525678f7ce6515eefbd44bcad9
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-10-28 04:51:06 +0300

    feat basic matrix operations

--- linalg.js
+++ linalg.js
@@ -189,6 +189,89 @@ class Matrix {
         this._m[i] = v.copy();
     }
     
+    get det() {
+        const [n, m] = this.dimension;
+        if (n !== m) {
+            throw new Error("Matrix must be square to compute determinant");
+        }
+        if (this._det === undefined) {
+            // Базовые случаи
+            if (n === 1) return this._m[0][0];
+            if (n === 2) {
+                return this._m[0][0] * this._m[1][1] - this._m[0][1] * this._m[1][0];
+            }
+        
+            // Рекурсивное вычисление
+            let det = 0;
+            for (let j = 0; j < n; j++) {
+                const minor = this._m.slice(1).map(row => row.filter((_, col) => col !== j));
+                const sign = (j % 2 === 0) ? 1 : -1;
+                det += sign * this._m[0][j] * (new Matrix(minor)).determinant();
+            }
+            this._det = det;
+        }
+        return this._det;
+    }
+    
+    get dimension() {
+        return [this._m.length, this._m[0].length];
+    }
+    
+    add(other) {
+        if (this.dimension[0] != other.dimension[0] || this.dimension[1] != other.dimension[1]) {
+            throw new WrongDimensionsError("matrices have different lengths");
+        }
+        let m = this.copy();
+
+        for (let i = 0; i < this.dimension[0]; i++) {
+            m.setVec(i, m.getVec(i).add(other.getVec(i)));
+        }
+        return m;
+    }
+
+    subtract(other) {
+        if (this.dimension[0] != other.dimension[0] || this.dimension[1] != other.dimension[1]) {
+            throw new WrongDimensionsError("matrices have different lengths");
+        }
+        let m = this.copy();
+
+        for (let i = 0; i < this.dimension[0]; i++) {
+            m.setVec(i, m.getVec(i).subtract(other.getVec(i)));
+        }
+        return m;
+    }
+
+    multiply(number) {
+        let m = this.copy();
+
+        for (let i = 0; i < this.dimension[0]; i++) {
+            m.setVec(i, m.getVec(i).multiply(number));
+        }
+        return m;
+    }
+
+    multiplyMatrix(other) {
+        const [nA, mA] = this.dimension;
+        const [nB, mB] = other.dimension;
+    
+        if (mA !== nB) {
+            throw new WrongDimensionsError("Number of columns of first matrix must equal number of rows of second matrix");
+        }
+    
+        // Результат имеет размер nA x mB
+        let m = Matrix.zero(nA, mB);
+    
+        for (let i = 0; i < nA; i++) {
+            for (let j = 0; j < mB; j++) {
+                let s = 0;
+                for (let k = 0; k < mA; k++) {
+                    s += this.get(i, k) * other.get(k, j);
+                }
+                m.set(i, j, s);
+            }
+        }
+        return m;
+    }
     copy() {
         let m = Array.from(this._m);
 
