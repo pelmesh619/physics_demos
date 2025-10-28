63a9c10a988cdf71dff489b7c61bc34c96d1a7e4
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-10-28 04:11:50 +0300

    feat LinearVector's basic operations

--- linalg.js
+++ linalg.js
@@ -48,6 +48,68 @@ class LinearVector {
         this._v[i] = x;
     }
     
+    get hypot() {
+        if (this._hypot === undefined) {
+            this._hypot = Math.hypot(...this._v);
+        }
+        return this._hypot;
+    }
+    
+    get length() {
+        return this._v.length;
+    }
+    
+    add(other) {
+        if (this.length != other.length) {
+            throw new WrongDimensionsError("vectors have different lengths");
+        }
+        let v = Array.from(this._v);
+        for (let i = 0; i < v.length; i++) {
+            v[i] += other.get(i);
+        }
+        return new LinearVector(v);
+    }
+
+    subtract(other) {
+        if (this.length != other.length) {
+            throw new WrongDimensionsError("vectors have different lengths");
+        }
+        let v = Array.from(this._v);
+        for (let i = 0; i < v.length; i++) {
+            v[i] -= other.get(i);
+        }
+        return new LinearVector(v);
+    }
+
+    multiply(number) {
+        let v = Array.from(this._v);
+
+        for (let i = 0; i < v.length; i++) {
+            v[i] *= number;
+        }
+        return new LinearVector(v);
+    }
+
+    scalarProduct(other) {
+        if (this.length != other.length) {
+            throw new WrongDimensionsError("vectors have different lengths");
+        }
+        let n = 0;
+        for (let i = 0; i < this.length; i++) {
+            n += this.get(i) * other.get(i)
+        }
+        return n;
+    }
+
+    do(func) {
+        let v = Array(this.length);
+
+        for (let i = 0; i < v.length; i++) {
+            v[i] = func(v[i]);
+        }
+        return new LinearVector(v);
+    }
+
     copy() {
         return new LinearVector(Array.from(this._v));
     }
